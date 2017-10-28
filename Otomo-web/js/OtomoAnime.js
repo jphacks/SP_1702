6bb2696e9b9547224adc1895c6953831b22e40b1
YuheiTakagawa <b1014223@fun.ac.jp>
2017-10-29 05:40:34 +0900

    [add]

---
+++ Otomo-web/js/OtomoAnime.js
@@ -0,0 +1,10 @@
+function bigSoul(){
+  $(function(){
+    $("#soul img").show().animate({
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%'
+    }, 500);
+  });
+}
